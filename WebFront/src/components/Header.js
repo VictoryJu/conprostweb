7fe4267643a6d8623f5794f9ffd9fcf69bbbe613
VictoryJu <kob0805@naver.com>
2021-05-09 21:42:54 +0900

    modify header

--- WebFront/src/components/Header.js
+++ WebFront/src/components/Header.js
@@ -1,25 +1,41 @@
 import React from 'react'
-import ReactHtmlParser from 'html-react-parser'; // 데이터 출력시 태그 없애주는 모듈
-import {useState} from 'react';
+ // 데이터 출력시 태그 없애주는 모듈
 const Header = () => {
 
-  const [viewContent, setViewContent] = useState([]);
-
-  return (
-    <header className="header">
-    <h2>게시판</h2>
-      <div className='content-container'>
-        {viewContent.map(element => 
-          <div>
-            <h2>{element.title}</h2>
-              <div>
-                {ReactHtmlParser(element.content)} 
-              </div>
-          </div>
-        )}
-      </div>
-  </header>
-  )
+return (
+<div>
+    <div class="row top">
+    <div class="col-9"></div>
+    <div class="col-1">회원가입</div>
+    <div class="col-1">문제신고</div>
+    <div class="col-1">알림</div>
+</div>
+<hr/>
+<div class="row">
+    <div class="nav">
+        <div class="col-3">
+            <a href="/" class="logo"> 
+                <img src="/img/logo.png" alt="logo" width="100%"/> 
+            </a>
+        </div>
+        <div class="col-6">
+            <form method="GET" action="/project" name="projectIdx">
+                <input type="text" placeholder="search" class="search" />
+            </form>
+        </div>
+        <div class="col-1">
+            <a href="#">About</a>
+        </div>
+        <div class="col-1">
+            <a href="#">Login</a>
+        </div>
+        <div class="col-1">
+            <a href="#">Mypage</a>
+        </div>
+    </div>
+</div>
+<hr/>
+</div>
+)
 }
-
 export default Header
