5e31fc3d33e4da223d86491c57c882c2771231ee
VictoryJu <kob0805@naver.com>
2021-05-16 23:06:10 +0900

    modify: footer, header & add Main, css

--- WebFront/src/components/Header.js
+++ WebFront/src/components/Header.js
@@ -1,41 +1,43 @@
 import React from 'react'
- // 데이터 출력시 태그 없애주는 모듈
+import logo from '../images/logo.png';
 const Header = () => {
 
 return (
-<div>
-    <div class="row top">
-    <div class="col-9"></div>
-    <div class="col-1">회원가입</div>
-    <div class="col-1">문제신고</div>
-    <div class="col-1">알림</div>
-</div>
-<hr/>
-<div class="row">
-    <div class="nav">
-        <div class="col-3">
-            <a href="/" class="logo"> 
-                <img src="/img/logo.png" alt="logo" width="100%"/> 
-            </a>
-        </div>
-        <div class="col-6">
-            <form method="GET" action="/project" name="projectIdx">
-                <input type="text" placeholder="search" class="search" />
-            </form>
-        </div>
-        <div class="col-1">
-            <a href="#">About</a>
-        </div>
-        <div class="col-1">
-            <a href="#">Login</a>
-        </div>
-        <div class="col-1">
-            <a href="#">Mypage</a>
+<header classNameName='header'>
+    <div>
+        <div className="row top">
+        <div className="col-9"></div>
+        <div className="col-1">회원가입</div>
+        <div className="col-1">문제신고</div>
+        <div className="col-1">알림</div>
+    </div>
+    <hr/>
+    <div className="row">
+        <div className="nav">
+            <div className="col-3">
+                <a href="/#" className="logo"> 
+                    <img src={logo} alt="logo" width="100%"/> 
+                </a>
+            </div>
+            <div className="col-6">
+                <form method="GET" action="/project" name="projectIdx">
+                    <input type="text" placeholder="search" className="search" />
+                </form>
+            </div>
+            <div className="col-1">
+                <a href="/#">About</a>
+            </div>
+            <div className="col-1">
+                <a href="/#">Login</a>
+            </div>
+            <div className="col-1">
+                <a href="/#">Mypage</a>
+            </div>
         </div>
     </div>
-</div>
-<hr/>
-</div>
+    <hr/>
+    </div>
+</header>
 )
 }
 export default Header
